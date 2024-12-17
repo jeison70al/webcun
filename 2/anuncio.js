b882793776f9cfa599a70bdef19c3d0feb5beec7
Jeison Alexander <140751981+jeison70al@users.noreply.github.com>
2024-12-17 17:34:43 -0500

    Primer commit

---
+++ 2/anuncio.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    //seleccionar el contenedor del anuncio y el botón de cierre
+    const anuncio = document.getElementById('anuncio');
+    const botonCerrar =document.getElementById('cerrar');
+
+    // Verificar si se seleccionaron correctamente
+    if (anuncio && botonCerrar) {
+        // Agregar el evento de clic al botón para cerrar el anuncio
+        botonCerrar.addEventListener('click', function () {
+            anuncio.style.display = 'none'; // Oculta el anuncio
+
+        });
+    }else {
+        console.error("El anuncio o el botón de cierre no se encontraron.");
+    }
+});
