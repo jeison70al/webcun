059b6ded11a91f5926c9a8edd96de2117f890b6f
Jeison Alexander <140751981+jeison70al@users.noreply.github.com>
2024-12-17 21:54:04 -0500

    python

---
+++ python/ejemplo1.py
@@ -0,0 +1,10 @@
+print("Bienvenido")
+numero1 = 80
+numero2 = 30
+estatura = 1.62
+edad = 34
+nombre = "JUAN PEREZ"
+aprobo = True
+
+resultado = numero1 + numero2
+print("el resultado de la operacion es", resultado)
